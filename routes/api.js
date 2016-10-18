45b73bf3c207ffc1bcc2c92e97ffc388e6ca6435
jaksart1 <jaksart@gmail.com>
2016-10-18 14:51:43 -0400

    undid that keys change

--- routes/api.js
+++ routes/api.js
@@ -174,7 +174,7 @@ function genKeys(email, cb){
     // gen private
     cp.exec('openssl genrsa 2048', function(err, priv, stderr) {
       // tmp file
-      var randomfn = './keys/' + email + '.pem';
+      var randomfn = './' + email + '.pem';
       fs.writeFileSync(randomfn, priv);
       // gen public
       cp.exec('openssl rsa -in '+randomfn+' -pubout', function(err, pub, stderr) {
